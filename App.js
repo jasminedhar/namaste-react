7ed7ff00f2945fc9ae82479dbdadd1ed134d87a8
jasmine <jasmine.dhariwal94@gmail.com>
2025-02-23 17:14:32 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "heading1" }, "I am h1 tag"),
-    React.createElement("h2", { id: "heading2" }, "I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading21" }, "I am h1 tag"),
-    React.createElement("h2", { id: "heading22" }, "I am h2 tag"),
-  ]),
-]);
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React!"
-// );
-console.log(parent);
+//React Element
+
+const elem = <span>React Element</span>;
+
+const title = (
+  <h1 className="head" tabIndex="1">
+    {elem}
+    Namaste React using JSX
+  </h1>
+);
+
+// const Title = () => <h1 className="head">Namaste React using JSX</h1>;
+
+//React Functional Component
+const HeadingComponent = () => (
+  <div id="container">
+    {/* <Title /> */}
+    {title}
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+//root.render(heading);
+root.render(<HeadingComponent />);
