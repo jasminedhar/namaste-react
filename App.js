cb5283cb70f21a60f758d3e4553c4830ba1d167e
jasmine <jasmine.dhariwal94@gmail.com>
2025-02-23 22:28:01 +0530

    Episode 4 - Half Part

--- App.js
+++ App.js
@@ -1,28 +1,75 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Element
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const elem = <span>React Element</span>;
+// const styleCard = {
+//   backgroundColor: "#f0f0f0",
+// };
 
-const title = (
-  <h1 className="head" tabIndex="1">
-    {elem}
-    Namaste React using JSX
-  </h1>
-);
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/e0vvulfbahjxjz6k4uwi"
+      />
+      <h3>Meghana Foods</h3>
+      <h4>Biryani,North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
 
-// const Title = () => <h1 className="head">Namaste React using JSX</h1>;
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
 
-//React Functional Component
-const HeadingComponent = () => (
-  <div id="container">
-    {/* <Title /> */}
-    {title}
-    <h1 className="heading">Namaste React Functional Component</h1>
-  </div>
-);
-
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //root.render(heading);
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
